3df727e17f3283f4817d36817ddf025229d5c8d3
coderganmeng <1814860168@qq.com>
2021-06-07 16:58:22 +0800

    注册小程序和页面

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,66 @@
 // miniprogram/pages/home/home.js
+// getApp()获取App()产生的实例对象
+// const app=getApp()
+// const name=app.globalData.name;
+// const age=app.globalData.age;
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  // ---------- 2.初始化数据 -------------------------
+  data:{
+    message:'哈哈哈哈',
+    list:[]
+  },
+  
+  // ----------- 1.监听页面的生命周期函数 ----------------
+  // 页面被加载出来
+  onLoad(){
+     console.log('onLoad');
+     wx.request({
+       url: 'http://152.136.185.210:7878/api/m5/recommend',
+       success:(res)=>{
+        console.log(res);
+        const data=res.data.data.list
+        this.setData({
+          list:data
+        })
+       }
+     })
+  },
+  // 页面初次渲染完成时
+  onReady(){
+    console.log('onReady');
+  },
+  // 页面显示出来时
+  onShow(){
+    console.log('onShow');
+  },
+  // 当页面隐藏起来时
+  onHide(){
+    console.log('onHide');
+  },
+  onUnload(){
+    console.log('onUnload');
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // ---------3.监听wxml中相关的一些时间----------
+  handleGetUserInfo(event){
+    // console.log(event);
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleViewClick(){
+    console.log('哈哈哈哈被点击了');
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  // --------- 4.监听其它时间----------
+  // 监听页面的滚动
+  onPageScroll(obj){
+    console.log(obj);
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  // 监听页面滚动到底部
+  onReachBottom(){
+    console.log('页面滚动到底部了');
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onPullDownRefresh(){
+    console.log('下拉刷新的事件');
   }
 })
